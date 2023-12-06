521acc0e50bc76ba0abb338bbbeb860fb93b672f
Mohammed Muti <mohammedmuti74@gamil.com>
2023-12-06 20:30:49 +0530

    commit

--- src/Components/common/Works/Works.jsx
+++ src/Components/common/Works/Works.jsx
@@ -6,19 +6,21 @@ const Works = () => {
     <>
       <section className="works">
         <h1 className="heading">Before & After Rug Cleaning Work</h1>
-        <div className="works-content">
-          <img
-            src={require("../../../Assets/Images/Gallery/Rug/20.webp")}
-            alt=""
-          />
-          <img
-            src={require("../../../Assets/Images/Gallery/Rug/1.webp")}
-            alt=""
-          />
-          <img
-            src={require("../../../Assets/Images/Gallery/Rug/6.webp")}
-            alt=""
-          />
+        <div className="works-content-wrapper">
+          <div className="works-content">
+            <img
+              src={require("../../../Assets/Images/Gallery/Rug/20.webp")}
+              alt=""
+            />
+            <img
+              src={require("../../../Assets/Images/Gallery/Rug/1.webp")}
+              alt=""
+            />
+            <img
+              src={require("../../../Assets/Images/Gallery/Rug/6.webp")}
+              alt=""
+            />
+          </div>
         </div>
         <Link to={"/gallery/rug-cleaning-gallery"}>
           <div className="more-images">
